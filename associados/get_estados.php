086dc40556ba0887a4486192664c3035355e809a
Felipe <felipeschandle@gmail.com>
2024-11-07 16:59:12 -0300

    commit

--- associados/get_estados.php
+++ associados/get_estados.php
@@ -12,19 +12,23 @@ if (!$conn) {
     exit;
 }
 
-// Consulta para obter os estados
-$query_estados = "SELECT id, nome, uf FROM estados";
-$result_estados = $conn->query($query_estados);
+try {
+    // Consulta para obter os estados
+    $query_estados = "SELECT id, nome, uf FROM estados";
+    $result_estados = $conn->query($query_estados);
 
-// Verifica se a consulta foi bem-sucedida
-if ($result_estados) {
-    // Se a consulta for bem-sucedida, transforma os resultados em um array associativo
-    $estados = $result_estados->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Retorna os dados em formato JSON
-    echo json_encode(['estados' => $estados]);
-} else {
-    // Se a consulta falhar, retorna um erro em formato JSON
-    echo json_encode(['error' => 'Erro ao consultar os estados']);
+    // Verifica se a consulta foi bem-sucedida
+    if ($result_estados) {
+        // Se a consulta for bem-sucedida, transforma os resultados em um array associativo
+        $estados = $result_estados->fetchAll(PDO::FETCH_ASSOC);
+        // Retorna os dados em formato JSON
+        echo json_encode(['estados' => $estados]);
+    } else {
+        // Se a consulta falhar, retorna um erro em formato JSON
+        echo json_encode(['error' => 'Erro ao consultar os estados']);
+    }
+} catch (Exception $e) {
+    // Em caso de erro, retorna a mensagem de erro
+    echo json_encode(['error' => 'Erro ao consultar estados: ' . $e->getMessage()]);
 }
 ?>
