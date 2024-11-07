e741ebb4c089240124257a4f8c8f40fc639859e3
Felipe <felipeschandle@gmail.com>
2024-11-06 22:22:31 -0300

    commit deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meu-usuario/sglb',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '761679fdbdaa2b47aeca0cf41c66e542205787f9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'meu-usuario/sglb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '761679fdbdaa2b47aeca0cf41c66e542205787f9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
