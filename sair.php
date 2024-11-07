e88f5e29e64df7c8c9c046b174ef41466a02c3fa
Felipe <felipeschandle@gmail.com>
2024-11-07 17:24:08 -0300

    commit

--- sair.php
+++ sair.php
@@ -4,4 +4,4 @@ session_start();
 unset($_SESSION['id'], $_SESSION['nome'], $_SESSION['sobrenome'], $_SESSION['email']);
 
 $_SESSION['msg'] = "<div class='alert alert-info' role='alert'>Deslogado com sucesso!</div>";
-header("Location: /SGLB/login");
+header("Location: /login");
