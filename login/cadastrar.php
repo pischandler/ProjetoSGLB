0a8aebbb503e3f076233ca09d1db531accdf1112
Felipe <felipeschandle@gmail.com>
2024-11-11 23:48:52 -0300

    commit

--- login/cadastrar.php
+++ login/cadastrar.php
@@ -76,9 +76,13 @@ if($btnCadUsuario) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Cadastrar</title>
     <link rel="stylesheet" href="login.css">
+       <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Icons -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="icon" href="../assets/imagemLosBravos.png" type="image/png">
 </head>
 <body>
@@ -125,5 +129,25 @@ if($btnCadUsuario) {
             </form>
         </div>
     </div>
+    <!-- Help Icon -->
+    <div class="help-icon" id="helpIcon">
+        <i class="bi bi-question-circle-fill"></i>
+    </div>
+    <!-- Help Modal -->
+    <div class="modal fade" id="helpModal" tabindex="-1" aria-labelledby="helpModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="helpModalLabel">Ajuda</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="helpContent">
+                    <!-- Help content will be dynamically inserted here -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src='js/custom.js'></script>
 </body>
 </html>
